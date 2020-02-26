6855bc13e5c0e065b66b7f6878e2fdf1172f8140
smartcentric <smartcentric@gmail.com>
2020-02-26 21:22:32 +0530

    csv to db

--- backend/routes/csvapi.js
+++ backend/routes/csvapi.js
@@ -2,44 +2,144 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const mongoose = require("mongoose");
-const csvCollection = require("../models/csv");
+// const csvCollection = require("../models/csv");
 var csvParser = require('csv-parse');
 
-// let filePath = require("../middleware/UserSample.csv");
+
 var csvfile = __dirname + "/UserSample.csv";
 
-//var stream = fs.createReadStream(csvfile);
+
+
+// router.post('/csvData', (req, res, next) => {
+
+//   var csv = new csvCollection();
+
+
+//   fs.readFile(csvfile , function(err, csvData) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     csvParser(csvData, { delimiter: ',' }, function(err, data) {
+//         if (err) {
+//              console.log(err);
+//         } else {
+//              console.log("Data", data);
+
+//             csvCollection.insertMany(data, (err, upData) => {
+//               if(err) {
+//               console.log("err", err);
+//               }
+//               else {
+//               console.log("Multiple documents inserted to Collection");
+//               }
+//             });
+//         }
+//     });
+// });
+// });
+
 
 router.post('/csvData', (req, res, next) => {
 
-  var csv = new csvCollection();
-  var uploadcsv = [];
+var lineList = fs.readFileSync(csvfile).toString().split('\n');
+lineList.shift(); // Shift the headings off the list of records.
 
-  fs.readFile(csvfile , function(err, csvData) {
+var schemaKeyList = ['UserName', 'FirstName', 'LastName', 'DisplayName', 'JobTitle', "Department", "OfficePhone",
+"MobilePhone", "Fax", 'Address', 'City', 'State', 'ZIP', 'Country'];
+
+const csvSchema = new mongoose.Schema({
+  UserName:{
+    type: String
+  },
+  FirstName: {
+    type: String
+  },
+  LastName: {
+    type: String
+  },
+  DisplayName: {
+    type: String
+  },
+  JobTitle: {
+    type: String
+  },
+  Department: {
+    type: String
+  },
+  OfficePhone: {
+    type: String
+  },
+  MobilePhone: {
+    type: String
+  },
+  Fax: {
+    type: String
+  },
+  Address: {
+    type: String
+  },
+  City: {
+    type: String
+  },
+  State: {
+    type: String
+  },
+  ZIP: {
+    type: Number
+  },
+  Country: {
+    type: String
+  }
+});
+
+var RepOppDoc = mongoose.model('csvData', csvSchema);
+
+
+function queryAllEntries () {
+  RepOppDoc.aggregate([
+      {$group: {_id: '$FirstName',
+
+              oppArray: {$push: {
+                LastName: '$LastName',
+                DisplayName: '$DisplayName',
+                JobTitle: '$JobTitle',
+                Department: '$Department',
+                OfficePhone: '$OfficePhone',
+                MobilePhone: '$MobilePhone',
+                Fax: '$Fax',
+                Address: '$Address',
+                City: '$City',
+                State: '$State',
+                ZIP: '$ZIP',
+                Country: '$Country'
+              }}
+              } }, (function(err, qDocList) {
+        // console.log(util.inspect(qDocList, false, 10));
+        process.exit(0);
+    })]);
+}
+
+function createDocRecurse (err) {
     if (err) {
         console.log(err);
+        process.exit(1);
     }
-    csvParser(csvData, { delimiter: ',' }, function(err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Data", data);
-            var jsonString = JSON.stringify(data);
-            console.log("json", jsonString);
+    if (lineList.length) {
+        var line = lineList.shift();
+        var doc = new RepOppDoc();
+        line.split(',').forEach(function (entry, i) {
+            doc[schemaKeyList[i]] = entry;
+        });
+        doc.save(createDocRecurse)
+    } else {
+        // After the last entry query to show the result.
+        queryAllEntries();
+    }
+}
 
-            csvCollection.insertMany(jsonString, (err, upData) => {
-              if(err) {
-                console.log("err", err);
-              }
-              else {
-                console.log("Multiple documents inserted to Collection");
-              }
-            });
-        }
-    });
-});
-});
+createDocRecurse(null);
 
 
+ });
 
 module.exports = router;
